a303658be39765404206e0831d631bd2e0311c9a
lymyr <176290600+lymyr@users.noreply.github.com>
2026-03-25 15:34:15 +0800

    completed js validation

---
+++ src/ValidationHelper.js
@@ -0,0 +1,80 @@
+const AddValidation = (() => {
+    const email = (form) => {
+        const re = /\S+@\S+\.\S+/
+        if (!/\S+/.test(form.email.value)) {
+            form.email.setCustomValidity("email can't be empty!")
+        }
+        else if (!re.test(form.email.value)) {
+            form.email.setCustomValidity("Input must be an email")
+        }
+        else {
+            form.email.setCustomValidity("")
+        }
+    }
+
+    const postal = (form) => {
+        let re = /placeholder/
+        let postalRange = ""
+        if (form.country.value == "ph") {
+            re = /^((0[4-9]\d{2})|([1-8]\d{3})|(9[0-7]\d{2})|(98(0\d|1[0-4])))$/
+            postalRange = "(0400 - 9814)";
+        }
+        else if (form.country.value == "ch") {
+            re = /^([1-9]\d\d\d)$/
+            postalRange = "(1000 - 9999)"
+        }
+        else if (form.country.value == "fr") {
+            re = /^((0[1-9]\d{3})|([1-8]\d{4})|(9[0-7]\d{3})|(98[0-8]\d{2}))$/
+            postalRange = "(01000 - 98899)";
+        }
+
+        if (!/\S+/.test(form.postal.value)) {
+            form.postal.setCustomValidity("Postal code can't be empty!")
+        }
+        else if (!re.test(form.postal.value)) {
+            form.postal.setCustomValidity(`Invalid postal code! ${postalRange}`)
+        }
+        else {
+            form.postal.setCustomValidity("")
+        }
+    }
+
+    const pwd = (form) => {
+        const re = /.{3,}/
+        if (!/\S+/.test(form.pwd.value)) form.pwd.setCustomValidity("Password can't be empty!")
+        else if (!re.test(form.pwd.value)) form.pwd.setCustomValidity("Password must be at least 3 characters!")
+        else form.pwd.setCustomValidity("")
+    }
+
+    const pwdConfirm = (form) => {
+        const re = /\S+/
+        if (!re.test(form['pwd-confirm'].value)) form['pwd-confirm'].setCustomValidity("Confirm Password can't be empty!")
+        else if (form['pwd-confirm'].value != form.pwd.value) form['pwd-confirm'].setCustomValidity("Passwords do not match!")
+        else form['pwd-confirm'].setCustomValidity("")
+    }
+    return { email, postal, pwd, pwdConfirm }
+})()
+
+const ReportValidation = (() => {
+    const email = (form) => {
+        AddValidation.email(form)
+        document.querySelector(`#email + span`).textContent = form.email.validationMessage
+    }
+    const postal = (form) => {
+        AddValidation.postal(form)
+        document.querySelector(`#postal + span`).textContent = form.postal.validationMessage
+    }
+
+    const pwd = (form) => {
+        AddValidation.pwd(form)
+        document.querySelector(`#pwd + span`).textContent = form.pwd.validationMessage
+    }
+
+    const pwdConfirm = (form) => {
+        AddValidation.pwdConfirm(form)
+        document.querySelector(`#pwd-confirm + span`).textContent = form['pwd-confirm'].validationMessage
+    }
+    return {email, postal, pwd, pwdConfirm}
+})()
+
+export { ReportValidation }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,72 @@
 import "./styles.css";
+import { ReportValidation } from "./ValidationHelper.js";
 
 const btn = document.querySelector("button");
+const form = document.querySelector("form");
 
-btn.addEventListener("click", e => {
-    e.preventDefault();
+const formInputs = ["email", "country", "postal", "pwd", "pwd-confirm"];
+
+const submitDetails = document.createElement("div");
+document.querySelector("body").append(submitDetails);
+for (let i = 0; i < formInputs.length; i++) {
+    submitDetails.append(document.createElement("p"));
+}
+
+
+function renderSubmitDetails() {
+    let i = 0;
+    submitDetails.setAttribute("style", "padding: 12px; margin: auto; margin-top: 2vh; border: 1px solid black");
+    formInputs.forEach(inp => {
+        submitDetails.childNodes[i].textContent = `${inp}: ${form[inp].value}`;
+        submitDetails.childNodes[i].setAttribute("style", "padding: 4px 12px")
+        i += 1;
+    })
+}
+
+function unrenderSubmitDetails() {
+    let i = 0;
+    submitDetails.setAttribute("style", "");
+    formInputs.forEach(() => {
+        submitDetails.childNodes[i].textContent = "";
+        submitDetails.childNodes[i].setAttribute("style", "");
+        i += 1;
+    })
+}
+
+function clearForm() {
+    formInputs.forEach(inp => {
+        if (inp != "country")
+        form[`${inp}`].value = ""
+    });
+}
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    unrenderSubmitDetails();
+
+    ReportValidation.email(form);
+    ReportValidation.postal(form);
+    ReportValidation.pwd(form)
+    ReportValidation.pwdConfirm(form)
+
+    if (form.checkValidity()) {
+        renderSubmitDetails();
+        clearForm();
+    }
 });
+
+form.email.addEventListener("blur", () => {
+    ReportValidation.email(form)
+})
+
+form.postal.addEventListener("blur", () => {
+    ReportValidation.postal(form)
+})
+
+form.pwd.addEventListener("blur", () => {
+    ReportValidation.pwd(form)
+})
+
+form['pwd-confirm'].addEventListener("blur", () => {
+    ReportValidation.pwdConfirm(form)
+})
